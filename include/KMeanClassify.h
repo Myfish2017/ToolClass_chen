b0cef78ef5135905ff64706e4bb9432951bf39a0
DESKTOP-R3ISDVL\chen <591139268@qq.com>
2018-01-11 15:45:05 +0800

    这个副本包含了目前有价值的东西，K均值模板分类和对线段类的修改以及之前的一个完整的识别算法

---
+++ include/KMeanClassify.h
@@ -0,0 +1,115 @@
+﻿#pragma once
+
+/*****************************************************************************
+* @function name : MinAvgDistanceIndex
+* @author : chen
+* @date : 2018/1/10 9:42
+* @inparam : 蕴含元素的容器，元素距离的定义函数
+* @outparam : 最能模拟这类容器的下标引用
+* @last change :
+*****************************************************************************/
+template<typename ElementType>
+void MinAvgDistance(const vector<ElementType> &element, size_t &index,
+	std::function<bool(const ElementType&, const ElementType&, double&)> function)
+{
+	double distance_sum = INT_MAX;
+	for (size_t i = 0; i < element.size(); ++i)
+	{
+		ElementType curr = element[i];
+		double curr_sum = 0;
+		for (size_t j = 0; j < element.size(); ++j)
+		{
+			double value;
+			function(curr, element[j], value);
+			curr_sum += value;
+		}
+		if (curr_sum < distance_sum)
+		{
+			index = i;
+			distance_sum = curr_sum;
+		}
+	}
+}
+
+
+/*****************************************************************************
+* @function name : KMeanLineSegment
+* @author : chen
+* @date : 2018/1/10 10:18
+* @describe : K_均值聚类使元素多分,这类k_均值算法的特点是质心是元素的其中一个
+* @last change :
+*****************************************************************************/
+template<typename ElementType>
+bool KMeanClassify(const vector<ElementType> &element_vec, vector<vector<ElementType>> &result
+	, size_t types, std::function<bool(const ElementType&, const ElementType&, double&)> function)
+{
+	if (element_vec.size() < types)
+	{
+		types = element_vec.size();
+	}
+	vector<vector<ElementType>> result_container;
+	vector<vector<size_t>> types_index_vec;
+	vector<size_t> centroid_vec;
+	types_index_vec.resize(types);
+	result_container.resize(types);
+	for (size_t i = 0; i < types; ++i)
+	{
+		result_container[i].push_back(element_vec[i]);
+		types_index_vec[i].push_back(i);
+		centroid_vec.push_back(i);
+	}
+	bool loop_flag = true;
+	while (loop_flag)
+	{
+		for (size_t i = 0; i < element_vec.size(); ++i)
+		{
+			bool centroid_flag = false;
+			for (size_t j = 0; j < centroid_vec.size(); ++j)
+			{
+				if (centroid_vec[j] == i)
+				{
+					centroid_flag = true;
+					break;
+				}
+			}
+			if (centroid_flag == true)
+			{
+				continue;
+			}
+			ElementType lineSegment_curr = element_vec[i];
+			int min_distance_index = 0;
+			double min_value = INT_MAX;
+			double value;
+			for (size_t j = 0; j < types; ++j)
+			{
+				function(lineSegment_curr, result_container[j][0], value);
+				if (value < min_value)
+				{
+					min_value = value;
+					min_distance_index = j;
+				}
+			}
+			result_container[min_distance_index].push_back(lineSegment_curr);
+			types_index_vec[min_distance_index].push_back(i);
+		}
+		size_t index = 0;
+		loop_flag = false;
+		centroid_vec.clear();
+		result = result_container;
+		for (size_t i = 0; i < types; ++i)
+		{
+			MinAvgDistance(result_container[i], index, function);
+			result_container[i][0] = result_container[i][index];
+			types_index_vec[i][0] = types_index_vec[i][index];
+			centroid_vec.push_back(types_index_vec[i][index]);
+			result_container[i].erase(result_container[i].begin() + 1, result_container[i].end());
+			types_index_vec[i].erase(types_index_vec[i].begin() + 1, types_index_vec[i].end());
+			if (index != 0)
+			{
+				loop_flag = true;
+			}
+		}
+	}
+	return true;
+}
+
--- include/LineSegment.h
+++ include/LineSegment.h
@@ -4,7 +4,13 @@
 #include <opencv2/core.hpp>
 #include <opencv2/highgui.hpp>
 #include <opencv2/imgproc.hpp>
+#include <vector>
+#include <map>
+#include <functional>
+using std::vector;
 using cv::Vec4i;
+using std::map;
+
 
 /*****************************************************************************
 * @class name : LineSegment
@@ -71,6 +77,183 @@ private:
 bool lineCmp(const LineSegment &lft, const LineSegment &rgh)
 {
 	return lft.Angle() < rgh.Angle();
+}
+
+/*****************************************************************************
+* @function name : SubLineSegmentByAngle
+* @author : chen
+* @date : 2018/1/10 8:21
+* @inparam : 2条线段
+* @outparam : 1个线段角度差值引用
+* @last change :
+*****************************************************************************/
+void SubLineSegmentByAngle(const LineSegment &lft, const LineSegment &rgh, double &value)
+{
+	value = std::min(std::abs(rgh.Angle() - lft.Angle()),
+		180 - std::abs(rgh.Angle()) - std::abs(lft.Angle()));
+}
+
+/*****************************************************************************
+* @function name : SubLineSegmentByDistance
+* @author : chen
+* @date : 2018/1/10 8:53
+* @inparam : 2条线段，和距离计算类型
+* @outparam : 距离引用
+* @last change :
+*****************************************************************************/
+bool SubLineSegmentParallelByDistance(const LineSegment &lft, const LineSegment &rgh, double &value)
+{
+	if (lft.Slope_existence() == true)
+	{
+		if (lft.Slope() != rgh.Slope())
+		{
+			return false;
+		}
+		double fz = sqrt(rgh.Slope()*rgh.Slope() + 1);
+		double rgh_intercept = -rgh[1] - rgh.Slope()*rgh[0];
+		double lft_intercept = -lft[1] - lft.Slope()*lft[0];
+		value = abs(rgh_intercept - lft_intercept) / fz;
+		return true;
+	}
+	else
+	{
+		value = abs(rgh[0] - lft[0]);
+		return true;
+	}
+	return false;
+}
+
+
+bool SubLineSegmentByDistance(const LineSegment &lft, const LineSegment &rgh, double &value)
+{
+	if (lft.Slope_existence() == false || rgh.Slope_existence() == false)
+	{
+		value = abs(rgh[0] - lft[0]);
+		return true;
+	}
+	else
+	{
+		/*double angle_ave = (lft.Angle() + rgh.Angle()) / 2;
+		double slope_ave = tan(angle_ave);*/
+
+		double slope_ave = (lft.Slope() + rgh.Slope()) / 2;
+		double fz = sqrt(slope_ave*slope_ave + 1);
+		double rgh_intercept = -rgh[1] - slope_ave*rgh[0];
+		double lft_intercept = -lft[1] - slope_ave*lft[0];
+		value = abs(rgh_intercept - lft_intercept) / fz;
+		return true;
+	}
+}
+
+/*****************************************************************************
+* @function name : MinAvgDistanceIndex
+* @author : chen
+* @date : 2018/1/10 9:42
+* @inparam : 线段_向量
+* @outparam : 最能模拟这组线段的线段下标引用
+* @last change :
+*****************************************************************************/
+void MinAvgDistance(const vector<LineSegment> &lineSegment, size_t &index,
+	std::function<bool(const LineSegment&, const LineSegment&, double&)> function)
+{
+	double distance_sum = INT_MAX;
+	for (size_t i = 0; i < lineSegment.size(); ++i)
+	{
+		LineSegment curr = lineSegment[i];
+		double curr_sum = 0;
+		for (size_t j = 0; j < lineSegment.size(); ++j)
+		{
+			double value;
+			function(curr, lineSegment[j], value);
+			curr_sum += value;
+		}
+		if (curr_sum < distance_sum)
+		{
+			index = i;
+			distance_sum = curr_sum;
+		}
+	}
+}
+
+
+/*****************************************************************************
+* @function name : KMeanLineSegment
+* @author : chen
+* @date : 2018/1/10 10:18
+* @describe : K_均值聚类使线段多分
+* @last change :
+*****************************************************************************/
+bool KMeanLineSegment(const vector<LineSegment> &lineSegment_vec, vector<vector<LineSegment>> &result
+	, size_t types, std::function<bool(const LineSegment&, const LineSegment&, double&)> function)
+{
+	if (lineSegment_vec.size() < types)
+	{
+		types = lineSegment_vec.size();
+	}
+	vector<vector<LineSegment>> result_container;
+	vector<vector<size_t>> types_index_vec;
+	vector<size_t> centroid_vec;
+	types_index_vec.resize(types);
+	result_container.resize(types);
+	for (size_t i = 0; i < types; ++i)
+	{
+		result_container[i].push_back(lineSegment_vec[i]);
+		types_index_vec[i].push_back(i);
+		centroid_vec.push_back(i);
+	}
+	bool loop_flag = true;
+	while (loop_flag)
+	{
+		for (size_t i = 0; i < lineSegment_vec.size(); ++i)
+		{
+			bool centroid_flag = false;
+			for (size_t j = 0; j < centroid_vec.size(); ++j)
+			{
+				if (centroid_vec[j] == i)
+				{
+					centroid_flag = true;
+					break;
+				}
+			}
+			if (centroid_flag == true)
+			{
+				continue;
+			}
+			LineSegment lineSegment_curr = lineSegment_vec[i];
+			int min_distance_index = 0;
+			double min_value = INT_MAX;
+			double value;
+			for (size_t j = 0; j < types; ++j)
+			{
+				function(lineSegment_curr, result_container[j][0], value);
+				if (value < min_value)
+				{
+					min_value = value;
+					min_distance_index = j;
+				}
+			}
+			result_container[min_distance_index].push_back(lineSegment_curr);
+			types_index_vec[min_distance_index].push_back(i);
+		}
+		size_t index = 0;
+		loop_flag = false;
+		centroid_vec.clear();
+		result = result_container;
+		for (size_t i = 0; i < types; ++i)
+		{
+			MinAvgDistance(result_container[i], index, function);
+			result_container[i][0] = result_container[i][index];
+			types_index_vec[i][0] = types_index_vec[i][index];
+			centroid_vec.push_back(types_index_vec[i][index]);
+			result_container[i].erase(result_container[i].begin() + 1, result_container[i].end());
+			types_index_vec[i].erase(types_index_vec[i].begin() + 1, types_index_vec[i].end());
+			if (index != 0)
+			{
+				loop_flag = true;
+			}
+		}
+	}
+	return true;
 }
 
 #endif
